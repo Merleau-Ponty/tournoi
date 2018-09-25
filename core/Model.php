544215dbccd4c5e21d8c12288a663ccd7fc698e5
Claude Fichet <fichetclaude@orange.fr>
2018-09-25 17:52:21 +0200

    Amelioration de la classe Model

--- core/Model.php
+++ core/Model.php
@@ -1,9 +1,7 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 25/09/2018 amelioration de la class
  */
 
 /**
