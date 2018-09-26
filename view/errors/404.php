a09ae4a937a9c04a85823d4a13824775218d9d0e
Claude Fichet <claudefichet@free.fr>
2018-09-26 16:31:21 +0200

    Mise à jour commentaires

--- view/errors/404.php
+++ view/errors/404.php
@@ -1,9 +1,7 @@
 <?php
 
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Page d'erreur par défaut
  */
 
 echo $message;
