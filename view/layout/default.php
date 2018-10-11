64ed520dcb871bf4ae4ff672e3e8f73932128687
Maxime GLOD <Maxime GLOD@SIO2-GLM>
2018-10-11 13:54:00 +0200

    Fix de la page d'accueil avec le default

--- view/layout/default.php
+++ view/layout/default.php
@@ -7,7 +7,7 @@
 <link href="<?= BASE_SITE ?>/css/style.css" rel="stylesheet" type="text/css">
 </head>
 <div id="header">
-<h1 id="bandeau"><img id="ea" src="<?= BASE_SITE ?>/img/ea.png" width="80" height="80" alt="Electronic Arts"/><a href="accueil_visiteur.php" style="text-decoration:none; color: white;">Gestion Tournoi Jeux Vidéos</a><a href="acceuil_organisateur.php"><img id="login" src="<?= BASE_SITE ?>/img/logo.png" width="190" height="80" alt=""/></a></h1>
+<h1 id="bandeau"><img id="ea" src="<?= BASE_SITE ?>/img/ea.png" width="80" height="80" alt="Electronic Arts"/><a href="home.php" style="text-decoration:none; color: white;">Gestion Tournoi Jeux Vidéos</a><a href="acceuil_organisateur.php"><img id="login" src="<?= BASE_SITE ?>/img/logo.png" width="190" height="80" alt=""/></a></h1>
 </div>
 <body>
 
