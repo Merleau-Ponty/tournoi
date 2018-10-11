8b84ae726de4a7b3b3a4a23068202aca1c50fb41
Maxime GLOD <Maxime GLOD@SIO2-GLM>
2018-10-11 16:42:36 +0200

    Début implémentation liste joueurs

--- view/tourn/liste_inscrit.php
+++ view/tourn/liste_inscrit.php
@@ -1,9 +1,9 @@
-<h1>Liste Secondaire</h1>
-<td>PSEUDO</td></tr>
+<h1 style="margin-left: 42%;">Liste Secondaire</h1>
+
 <?php foreach ($joueurs as $ligne):?>
 <?php if ($ligne->ETAT==0){
     echo '
-    <table style="margin-left: 55%;">
+    <table style="margin-left: 48%;">
     <tr class="tableau arrondi ">
 
         <td class="tableau arrondi">'.$ligne->PSEUDO.'</td>
@@ -17,13 +17,13 @@ else {
     }
 ?>
 <?php endforeach;?>
-  <h1>Liste Principale</h1>
-<td>PSEUDO</td></tr>  
+  <h1 style="margin-left: 42%;">Liste Principale</h1>
+
  <?php foreach ($joueurs as $ligne):?>
     
 <?php if ($ligne->ETAT==1){
     echo '
-    <table style="margin-left: 55%;">
+    <table style="margin-left: 48%;">
     <tr class="tableau arrondi ">
 
 
