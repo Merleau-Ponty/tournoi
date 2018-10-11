83978992337dbd1ed6c642852299ae1ed71bfc1e
Maxime GLOD <Maxime GLOD@SIO2-GLM>
2018-10-11 16:58:17 +0200

    Lister les joueurs complet.

--- view/tourn/liste_inscrit.php
+++ view/tourn/liste_inscrit.php
@@ -3,11 +3,10 @@
 <?php foreach ($joueurs as $ligne):?>
 <?php if ($ligne->ETAT==0){
     echo '
-    <table style="margin-left: 48%;">
-    <tr class="tableau arrondi ">
 
-        <td class="tableau arrondi">'.$ligne->PSEUDO.'</td>
+    <table style="margin-left: 48%;" style="margin-left: 48%;">
 
+       <th> <td class="tableau arrondi" style="margin-left: 48%; ">'.$ligne->PSEUDO.'</td>
 
     </tr>
     </table>';
