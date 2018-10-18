8d3971a4156fb02962d9ec720596266b0a6382df
Maxime GLOD <Maxime GLOD@SIO2-GLM>
2018-10-18 15:17:12 +0200

    Liste des inscrits

--- view/tourn/liste_inscrit.php
+++ view/tourn/liste_inscrit.php
@@ -1,32 +1,14 @@
-<h1 style="margin-left: 42%;">Liste Secondaire</h1>
-
-<?php foreach ($joueurs as $ligne):?>
-<?php if ($ligne->ETAT==0){
-    echo '
-
-    <table style="margin-left: 48%;" style="margin-left: 48%;">
-
-       <th> <td class="tableau arrondi" style="margin-left: 48%; ">'.$ligne->PSEUDO.'</td>
-
-    </tr>
-    </table>';
-    
-}
-else {
-    }
-?>
-<?php endforeach;?>
-  <h1 style="margin-left: 42%;">Liste Principale</h1>
+ <h1 style="text-align: center;">Liste Principale</h1>
 
  <?php foreach ($joueurs as $ligne):?>
     
 <?php if ($ligne->ETAT==1){
     echo '
     <table style="margin-left: 48%;">
-    <tr class="tableau arrondi ">
+    <tr class="tableau arrondi " >
 
 
-        <td class="tableau arrondi">'.$ligne->PSEUDO.'</td>
+        <td class="tableau arrondi" style="text-align: center;">'.$ligne->PSEUDO.'</td>
      
 
     </tr>
@@ -37,6 +19,32 @@ else {
     }
 ?>
 <?php endforeach;?>
+<h1 style="text-align: center;">Liste Secondaire</h1>
+
+<?php foreach ($joueurs as $ligne):?>
+<?php if ($ligne->ETAT==0){
+    echo '
+
+    <table style="margin-left: 48%;" >
+
+       <th> <td class=" arrondi" style="margin-left: 48%; font-family:verdana, sans-serif;
+    font-size:100%;
+    color:orange;
+    text-align:center;
+    background-color:transparent;
+    border-color:orange;
+    border-style:solid;
+    border-width:3px;">'.$ligne->PSEUDO.'</td>
+
+    </tr>
+    </table>';
+    
+}
+else {
+    }
+?>
+<?php endforeach;?>
+ 
        
 
 
