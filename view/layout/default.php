7cc74f51b45fb65fa2e9cc424227f2ebc6e2c500
Nolan <Nolan@DESKTOP-CK6K3TI>
2018-10-18 15:32:54 +0200

    Ajout de bootstrap

--- view/layout/default.php
+++ view/layout/default.php
@@ -5,6 +5,8 @@
 <meta charset="utf-8">
 <title>Gestion Tournoi Jeux Vidéos</title>
 <link href="<?= BASE_SITE ?>/css/style.css" rel="stylesheet" type="text/css">
+<!-- Lien pour Bootstrap -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <div id="header">
 <h1 id="bandeau"><img id="ea" src="<?= BASE_SITE ?>/img/ea.png" width="80" height="80" alt="Electronic Arts"/><a href="home" style="text-decoration:none; color: white;">Gestion Tournoi Jeux Vidéos</a><a href="acceuil_organisateur"><img id="login" src="<?= BASE_SITE ?>/img/logo.png" width="190" height="80" alt=""/></a></h1>
