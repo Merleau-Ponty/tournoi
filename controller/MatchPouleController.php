ac30af47e03ee62344f91a3c8296d7c67d3739c6
Nolan <Nolan@DESKTOP-CK6K3TI>
2018-11-06 16:11:06 +0100

    Commit Matchs de poule

--- controller/MatchPouleController.php
+++ controller/MatchPouleController.php
@@ -163,7 +163,42 @@ class MatchPouleController extends Controller {
         $this->set($d);
     } 
     
+    //Liste les matchs de poules
     public function liste($num_poule){
+        
+
+
+         
+        //Permet de lister les matchs dans une poule avec le pseudo des joueurs, leur scores et l'horaire des matchs.
+        $modelmatch_poule = $this->loadModel('JoueursScoresMatch_poule');
+        $projection ='match_poule.numero, match_poule.id_match, match_poule.date_heure, GROUP_CONCAT(joueurs.PSEUDO) AS joueurs, GROUP_CONCAT(CONCAT(joueurs.PSEUDO, " ",scores.SCORE)) AS SCORES';
+        $groupby = 'match_poule.id_match';
+        $condition = array('numero'=>$num_poule);
+        $params = array( 'projection' => $projection,'groupby'=>$groupby,'conditions' => $condition);
+        $listematch = $modelmatch_poule->find($params);
+        $idmatch = 'match_poule.id_match';
+        $d['match_poule'] = $listematch;
+        $d['num_poule'] = $num_poule;
+        $d['num_match'] = $idmatch;
+        
+       $this->set($d);
+        
+       
+       
+        /*
+        
+         SQL : Permet d'afficher dans chaque poule, chaque match, avec l'horaire du match et de ces joueurs
+        select match_poule.numero, match_poule.id_match, match_poule.date_heure, GROUP_CONCAT(joueurs.PSEUDO) AS joueurs, GROUP_CONCAT(CONCAT(joueurs.PSEUDO, ' ',IFNULL(scores.SCORE,0))) AS SCORES
+        FROM joueurs 
+        INNER JOIN scores on scores.ID_JOUEUR = joueurs.ID_JOUEUR 
+        INNER JOIN match_poule ON match_poule.ID_MATCH = scores.ID_MATCH 
+        GROUP BY match_poule.id_match
+        ORDER BY match_poule.numero
+
+        */
+       
+        
+  
         
     }    
 }
--- view/matchpoule/liste.php
+++ view/matchpoule/liste.php
@@ -1,2 +1,22 @@
-ca marche
+
+<!--On affiche les lignes de la table messages $d['messages']-->
+<h1>Poule n°<?=$num_poule?></h1>
+
+<table  border="1px">
+     <tr>
+        <th>Match n°</th>
+        <th>Horaire</th>
+        <th>Joueurs</th>
+        <th>Scores</th>
+    </tr>
+    <!--<?php print_r ($match_poule);?>-->
+    <?php foreach ($match_poule as $ligne):?>
+   <tr>
+        <td><?=$ligne->ID_MATCH;?></td>
+        <td><?=$ligne->DATE_HEURE;?></td>
+        <td><?=$ligne->joueurs;?></td>
+        <td><?=$ligne->SCORES;?></td>
+    </tr>
+    <?php endforeach;?>
+</table>
 
