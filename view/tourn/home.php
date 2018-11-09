80e32d2895525bff0f801d26e8326e9aa76c77ee
Maxime GLOD <Maxime GLOD@SIO2-GLM>
2018-11-09 08:03:46 +0100

    fix des liens dans le home de tourn

--- view/tourn/home.php
+++ view/tourn/home.php
@@ -1,8 +1,8 @@
 <body id="appli">
     <p id="label">
-        <span id="inscription_logol"><a href="inscription_visiteur.php" style="text-decoration:none; color: black;">S’inscrire en ligne</span></a> 
-<span id="liste_joueurl"><a href="liste_inscrit.php" style="text-decoration:none; color: black;">Lister les Inscrits</span></a>
-<span id="liste_tournoil"><a href="liste_match.php" style="text-decoration:none; color: black;">Lister les matchs</span></a>
+        <span id="inscription_logol"><a href="<?= BASE_URL ?>/joueur/inscription_joueur" style="text-decoration:none; color: black;">S’inscrire en ligne</span></a> 
+<span id="liste_joueurl"><a href="<?= BASE_URL ?>/tourn/liste_inscrit" style="text-decoration:none; color: black;">Lister les Inscrits</span></a>
+<span id="liste_tournoil"><a href="<?= BASE_URL ?>/tourn/liste_match" style="text-decoration:none; color: black;">Lister les matchs</span></a>
     </p>
     <p id="choixjoueur">
     <span id="inscription_logo">    
